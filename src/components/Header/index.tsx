70f5db84cd84680984f52a4289124798a9fdb4bd
Anderson Santos <anderson.santos@eunerd.com.br>
2022-04-24 00:27:38 -0300

    Configurando o Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Container, Content } from "./styles";
 
+
 export function Header(){
+
+    const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
+    function handleModalIsOpen(){
+        setModalIsOpen(true);
+    }
+
+    function handleModalIsClose(){
+        setModalIsOpen(false);
+    }
+
     return(
         <Container>
             <Content>
                 <h1>KeepRood</h1>
                 <input type="search" name="" id="" placeholder="Pesquisar"/>
-                <button type="submit">Nova Nota</button>
+                <button type="submit" onClick={handleModalIsOpen} >Nova Nota</button>
             </Content>
+            <Modal isOpen={modalIsOpen} onRequestClose={handleModalIsClose} />
         </Container>
     );
 }
