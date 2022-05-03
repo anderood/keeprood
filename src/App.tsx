631a67f9b9804ca3f028fdb9e012c425763e0881
Anderson Santos <anderson.santos@eunerd.com.br>
2022-05-02 23:57:16 -0300

    Movendo o Modal para o App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 function App() {
+
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
+  function handleModalIsOpen(){
+      setModalIsOpen(true);
+  }
+
+  function handleModalIsClose(){
+      setModalIsOpen(false);
+  }
   return (
     <div className="App">
       <GlobalStyle/>
-      <Header/>
+      <Header 
+        onOpenNewTransactionModal={handleModalIsOpen}
+        isOpen={modalIsOpen}
+        onRequestClose={handleModalIsClose}
+        />
       <Dashboard />
     </div>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,23 @@
-import { useState } from 'react';
 import Modal from 'react-modal';
 import { Container, Content } from "./styles";
 
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+    isOpen: boolean,
+    onRequestClose: ()=> void;
+}
 
-export function Header(){
 
-    const [ modalIsOpen, setModalIsOpen ] = useState(false);
-
-    function handleModalIsOpen(){
-        setModalIsOpen(true);
-    }
-
-    function handleModalIsClose(){
-        setModalIsOpen(false);
-    }
+export function Header({onOpenNewTransactionModal, isOpen, onRequestClose }: HeaderProps){
 
     return(
         <Container>
             <Content>
                 <h1>KeepRood</h1>
                 <input type="search" name="" id="" placeholder="Pesquisar"/>
-                <button type="submit" onClick={handleModalIsOpen} >Nova Nota</button>
+                <button type="submit" onClick={onOpenNewTransactionModal} >Nova Nota</button>
             </Content>
-            <Modal isOpen={modalIsOpen} onRequestClose={handleModalIsClose} />
+            <Modal isOpen={isOpen} onRequestClose={onRequestClose} />
         </Container>
     );
 }
