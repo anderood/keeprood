ce2fb92d5265c7e8256c525e165f62bc9374eaf2
Anderson <anderood@hotmail.com>
2025-06-12 18:46:44 -0300

    criado componente de nova nota

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Nav from "/src/Components/Nav/index.jsx";
+import NewNote from "/src/Components/NewNote/index.jsx";
 function App() {
   return (
     <>
         <Nav />
+        <NewNote />
     </>
   )
 }
---
+++ src/Components/NewNote/index.jsx
@@ -0,0 +1,27 @@
+import { FaCheckSquare, FaEdit, FaImage } from 'react-icons/fa'; // ou outros ícones de sua escolha
+
+export default function NewNote(){
+    return(
+        <div className="container mx-auto bg-light">
+           <div className="row p-5">
+               <div className="" style={{ width: '600px'}}>
+                   <div className="card shadow-sm border-0 rounded px-3 py-2 d-flex align-items-center flex-row">
+                       <input
+                           type="text"
+                           className="form-control border-0 shadow-none"
+                           placeholder="Criar uma nota..."
+                           style={{ flex: 1 }}
+                       />
+
+                       <div className="d-flex align-items-center gap-3 ms-3">
+                           <FaCheckSquare className="text-secondary" role="button" />
+                           <FaEdit className="text-muted" role="button" />
+                           <FaImage className="text-secondary" role="button" />
+                       </div>
+                   </div>
+               </div>
+
+           </div>
+        </div>
+    );
+}
