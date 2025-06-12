46725db2acf22cdae0f569c32ddab07b79754d6d
Anderson <anderood@hotmail.com>
2025-06-12 20:05:42 -0300

    Criado container para recebimento dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Nav from "/src/Components/Nav/index.jsx";
 import NewNote from "/src/Components/NewNote/index.jsx";
+import Container from "/src/Components/Container/index.jsx";
 function App() {
   return (
     <>
         <Nav />
         <NewNote />
+        <Container />
     </>
   )
 }
--- src/Components/NewNote/index.jsx
+++ src/Components/NewNote/index.jsx
@@ -1,9 +1,9 @@
-import { FaCheckSquare, FaEdit, FaImage } from 'react-icons/fa'; // ou outros ícones de sua escolha
+import { FaCheckSquare, FaEdit, FaImage } from 'react-icons/fa';
 
 export default function NewNote(){
     return(
         <div className="container mx-auto bg-light">
-           <div className="row p-5">
+           <div className="row p-5 d-flex justify-content-center">
                <div className="" style={{ width: '600px'}}>
                    <div className="card shadow-sm border-0 rounded px-3 py-2 d-flex align-items-center flex-row">
                        <input
