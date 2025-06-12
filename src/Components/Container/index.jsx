6a121bff2934e4ad87897548c55748e38a0cd741
Anderson <anderood@hotmail.com>
2025-06-12 20:47:21 -0300

    Teste para visualizar quantidade de cards

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -17,6 +17,140 @@ function Container(){
                        <Card.Link href="#">Another Link</Card.Link>
                    </Card.Body>
                </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+               <Card style={{ width: '18rem', height: '14rem' }}>
+                   <Card.Body>
+                       <Card.Title>Leitura de Livros - 2024</Card.Title>
+                       <Card.Text>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Card.Link href="#">Card Link</Card.Link>
+                       <Card.Link href="#">Another Link</Card.Link>
+                   </Card.Body>
+               </Card>
+
+
 
            </div>
         </div>
